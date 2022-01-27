4a8c4eee1190b953e5b1a89bd09bf5de0b4e138a
dillan-groce <90156434+dillan-groce@users.noreply.github.com>
2022-01-26 19:53:42 -0500

    Delete ElevenNote.WebAPI directory

--- ElevenNote.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ElevenNote.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ElevenNote.WebAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElevenNote.WebAPI.WebApiApplication" Language="C#" %>
